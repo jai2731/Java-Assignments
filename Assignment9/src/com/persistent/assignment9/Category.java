a26648c6092e6d1a4d9dc0a0beb6ec4abdfa1d18
jayaprakash <jayaprakash_g@persistent.com>
2021-03-04 12:46:12 +0530

    First commit on Assignment9

---
+++ Assignment9/src/com/persistent/assignment9/Category.java
@@ -0,0 +1,47 @@
+package com.persistent.assignment9;
+
+import java.io.Serializable;
+
+public class Category implements Serializable  
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int catID;
+	private String catName;
+	
+	Category()
+	{
+		catID=0;
+		catName="";
+	}
+
+	public int getCatID() 
+	{
+		return catID;
+	}
+
+	public void setCatID(int catID) 
+	{
+		this.catID = catID;
+	}
+
+	public String getCatName()
+	{
+		return catName;
+	}
+
+	public void setCatName(String catName) 
+	{
+		this.catName = catName;
+		if(catName.equalsIgnoreCase("horror"))
+			this.setCatID(2);
+		if(catName.equalsIgnoreCase("thriller"))
+			this.setCatID(1);
+		if(catName.equalsIgnoreCase("suspense"))
+			this.setCatID(3);
+	}
+	
+	
+}
+
